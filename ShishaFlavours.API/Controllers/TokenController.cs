6cadc975d8996c90b1b918ea06f78c6266db9f41
ozonebg <muzunov875@gmail.com>
2018-07-12 18:15:42 +0300

    Added huge decoupling to the project

---
+++ ShishaFlavours.API/Controllers/TokenController.cs
@@ -0,0 +1,88 @@
+﻿namespace ShishaFlavoursAPI.Controllers
+{
+    using System;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.IdentityModel.Tokens;
+    using ShishaFlavoursAPI.Models;
+    using ShishaFlavoursAPI.RequestModels;
+
+    public class TokenController : Controller
+    {
+        private IConfiguration config;
+
+        private SignInManager<User> _signInManager = null;
+
+        public TokenController(IConfiguration config, SignInManager<User> signInManager)
+        {
+            this.config = config;
+            this._signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> CreateToken([FromBody]LoginModel login)
+        {
+            IActionResult response = Unauthorized();
+
+            var user = await Authenticate(login);
+
+            if (user != null)
+            {
+                var tokenString = BuildToken(user);
+                response = Ok(new { token = tokenString });
+            }
+
+            return response;
+        }
+
+        private string BuildToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Jti, user.Id)
+            };
+
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(config["Jwt:Issuer"],
+                 config["Jwt:Issuer"],
+                 claims,
+                 expires: DateTime.Now.AddMinutes(30),
+                 signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private async Task<User> Authenticate(LoginModel login)
+        {
+            User user = await _signInManager.UserManager.FindByNameAsync(login.Username);
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, login.Password, false);
+
+            if (result.Succeeded)
+            {
+                return user;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ShishaFlavours.API/RequestModels/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ShishaFlavoursAPI.RequestModels
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
