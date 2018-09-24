8f31b6625875e1d7eacb0f3b5188b8892b0d3d55
youngwolf <muzunov875@gmail.com>
2018-09-24 12:33:35 +0300

    refactored some actions to separate controller

---
+++ ShishaFlavours.API/Controllers/FlavourCombinationsStatisticsController.cs
@@ -0,0 +1,44 @@
+﻿namespace ShishaFlavours.API.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using ShishaFlavours.Services.Interfaces;
+    using System.Threading.Tasks;
+
+    public class FlavourCombinationsStatisticsController : Controller
+    {
+        private readonly IFlavourCombinationsService flavourCombinationsService = null;
+
+        public FlavourCombinationsStatisticsController(IFlavourCombinationsService flavourCombinationsService)
+        {;
+            this.flavourCombinationsService = flavourCombinationsService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetFlavourCombinationContaining(int flavourId)
+        {
+            var flavourCombosContainingFlavour = await flavourCombinationsService.GetFlavourCombinationsContaining(flavourId);
+
+            return new JsonResult(flavourCombosContainingFlavour);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetTop10FlavourCombinations()
+        {
+            var top10 = await flavourCombinationsService.GetTop10Combinations();
+
+            return new JsonResult(top10);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetCombinationsByVotes(bool isDescending = true)
+        {
+            var combosByVotes = await flavourCombinationsService.GetCombinationsByVotes(isDescending);
+
+            return new JsonResult(combosByVotes);
+        }
+    }
+}
